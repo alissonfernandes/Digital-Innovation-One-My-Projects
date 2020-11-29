f952e37edbee2aa0ff1f31a22012376aa3661bd7
Alisson Fernandes <alissonfernandes200@gmail.com>
2020-11-29 17:48:42 -0300

    Snake Game - Cobrinha
    Criando Cobrinha.

--- Snake/script.js
+++ Snake/script.js
@@ -3,9 +3,27 @@ let context = canvas.getContext('2d');
 
 let box = 35;
 
-criarBG();
+//Localização inicial cobrinha
+let snake = [];
+snake[0] = {
+    x: 8*box,
+    y: 8*box
+}
 
+criarBG();
+criarCobrinha();
+
+/** Criar background */
 function criarBG(){
     context.fillStyle = "Lightgreen";/**Cor de fundo */
     context.fillRect(0, 0, 16*box, 16*box);
+}
+
+/** Criar Cobrinha */
+function criarCobrinha(){
+
+    for(i=0; i< snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
